17746b3135810174f14e2a9415043d9e6ae46349
deaxparadox <nitish136900kushwaha@gmail.com>
2022-09-21 15:01:11 +0530

    extensive

---
+++ static/js/caret_up_down.js
@@ -0,0 +1,180 @@
+// // 
+// //  
+// //  PYTHON
+// // 
+// //
+// // Basics
+// let PlusMinusPythonBasics = document.getElementById("PlusMinusPythonBasics")
+// PlusMinusPythonBasics.addEventListener("click", function () {
+//     let Change = this.firstElementChild.firstElementChild.firstElementChild;
+
+//     if (Change.classList.contains("bi-caret-right-fill")) {
+//         Change.classList.remove("bi-caret-right-fill")
+//         Change.classList.add("bi-caret-up-fill")
+//     } else {
+//         Change.classList.remove("bi-caret-up-fill")
+//         Change.classList.add("bi-caret-right-fill")
+//     }
+// });
+// // 
+// //
+// // Advance
+// let PlusMinusPythonAdvance = document.getElementById("PlusMinusPythonAdvance")
+// PlusMinusPythonAdvance.addEventListener("click", function () {
+//     let Change = this.firstElementChild.firstElementChild.firstElementChild;
+
+//     if (Change.classList.contains("bi-caret-right-fill")) {
+//         Change.classList.remove("bi-caret-right-fill")
+//         Change.classList.add("bi-caret-up-fill")
+//     } else {
+//         Change.classList.remove("bi-caret-up-fill")
+//         Change.classList.add("bi-caret-right-fill")
+//     }
+// });
+
+
+
+// // 
+// //  
+// //  RUST
+// // 
+// //
+// // Basics
+// let PlusMinusRustBasics = document.getElementById("PlusMinusRustBasics")
+// PlusMinusRustBasics.addEventListener("click", function () {
+//     let Change = this.firstElementChild.firstElementChild.firstElementChild;
+
+//     if (Change.classList.contains("bi-caret-right-fill")) {
+//         Change.classList.remove("bi-caret-right-fill")
+//         Change.classList.add("bi-caret-up-fill")
+//     } else {
+//         Change.classList.remove("bi-caret-up-fill")
+//         Change.classList.add("bi-caret-right-fill")
+//     }
+// });
+// // 
+// //
+// // Ownership
+// let PlusMinusRustOwnership = document.getElementById("PlusMinusRustOwnership")
+// PlusMinusRustOwnership.addEventListener("click", function () {
+//     let Change = this.firstElementChild.firstElementChild.firstElementChild;
+
+//     if (Change.classList.contains("bi-caret-right-fill")) {
+//         Change.classList.remove("bi-caret-right-fill")
+//         Change.classList.add("bi-caret-up-fill")
+//     } else {
+//         Change.classList.remove("bi-caret-up-fill")
+//         Change.classList.add("bi-caret-right-fill")
+//     }
+// }); 
+// //
+// // Structure
+// let PlusMinusRustStructure = document.getElementById("PlusMinusRustStructure")
+// PlusMinusRustStructure.addEventListener("click", function () {
+//     let Change = this.firstElementChild.firstElementChild.firstElementChild;
+
+//     if (Change.classList.contains("bi-caret-right-fill")) {
+//         Change.classList.remove("bi-caret-right-fill")
+//         Change.classList.add("bi-caret-up-fill")
+//     } else {
+//         Change.classList.remove("bi-caret-up-fill")
+//         Change.classList.add("bi-caret-right-fill")
+//     }
+// }); 
+// //
+// // Enum
+// let PlusMinusRustEnum = document.getElementById("PlusMinusRustEnum")
+// PlusMinusRustEnum.addEventListener("click", function () {
+//     let Change = this.firstElementChild.firstElementChild.firstElementChild;
+
+//     if (Change.classList.contains("bi-caret-right-fill")) {
+//         Change.classList.remove("bi-caret-right-fill")
+//         Change.classList.add("bi-caret-up-fill")
+//     } else {
+//         Change.classList.remove("bi-caret-up-fill")
+//         Change.classList.add("bi-caret-right-fill")
+//     }
+// }); 
+// // 
+// //  
+// // Collection
+// let PlusMinusRustCollection = document.getElementById("PlusMinusRustCollection")
+// PlusMinusRustCollection.addEventListener("click", function () {
+//     let Change = this.firstElementChild.firstElementChild.firstElementChild;
+
+//     if (Change.classList.contains("bi-caret-right-fill")) {
+//         Change.classList.remove("bi-caret-right-fill")
+//         Change.classList.add("bi-caret-up-fill")
+//     } else {
+//         Change.classList.remove("bi-caret-up-fill")
+//         Change.classList.add("bi-caret-right-fill")
+//     }
+// });
+// // 
+// // 
+// // 
+// //  
+// // Modules
+// let PlusMinusRustModules = document.getElementById("PlusMinusRustModules")
+// PlusMinusRustModules.addEventListener("click", function () {
+//     let Change = this.firstElementChild.firstElementChild.firstElementChild;
+
+//     if (Change.classList.contains("bi-caret-right-fill")) {
+//         Change.classList.remove("bi-caret-right-fill")
+//         Change.classList.add("bi-caret-up-fill")
+//     } else {
+//         Change.classList.remove("bi-caret-up-fill")
+//         Change.classList.add("bi-caret-right-fill")
+//     }
+// }); 
+// // 
+// //  
+// // Projects
+// let PlusMinusRustProjects = document.getElementById("PlusMinusRustProjects")
+// PlusMinusRustProjects.addEventListener("click", function () {
+//     let Change = this.firstElementChild.firstElementChild.firstElementChild;
+
+//     if (Change.classList.contains("bi-caret-right-fill")) {
+//         Change.classList.remove("bi-caret-right-fill")
+//         Change.classList.add("bi-caret-up-fill")
+//     } else {
+//         Change.classList.remove("bi-caret-up-fill")
+//         Change.classList.add("bi-caret-right-fill")
+//     }
+// }); 
+// // 
+// //  
+// // Advance
+// let PlusMinusRustAdvance = document.getElementById("PlusMinusRustAdvance")
+// PlusMinusRustAdvance.addEventListener("click", function () {
+//     let Change = this.firstElementChild.firstElementChild.firstElementChild;
+
+//     if (Change.classList.contains("bi-caret-right-fill")) {
+//         Change.classList.remove("bi-caret-right-fill")
+//         Change.classList.add("bi-caret-up-fill")
+//     } else {
+//         Change.classList.remove("bi-caret-up-fill")
+//         Change.classList.add("bi-caret-right-fill")
+//     }
+// });
+// // 
+// // Generic Type, Trait, and Lifetimes
+// (function () {
+//     let PlusMinusRustGenericTraitLife = document.getElementById("PlusMinusRustGenericTraitLife")
+//     PlusMinusRustGenericTraitLife.addEventListener("click", function () {
+//         let Change = this.firstElementChild.firstElementChild.firstElementChild;
+//         // console.log(Change);
+//         // console.log(document.getElementById("PlusMinusRustGenericTraitLife").firstElementChild.firstElementChild)
+//         // console.log(Change.classList.contains("bi-caret-right-fill"));
+
+//         if (Change.classList.contains("bi-caret-right-fill")) {
+//             // console.log("contain down")
+//             Change.classList.remove("bi-caret-right-fill")
+//             Change.classList.add("bi-caret-up-fill")
+//         } else {
+//             // console.log("contain up")
+//             Change.classList.remove("bi-caret-up-fill")
+//             Change.classList.add("bi-caret-right-fill")
+//         }
+//     });
+// })();
---
+++ tools/static/js/index.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+    // Adjust column accout to width
+    let Width = window.innerWidth;
+    if (Width < 1200) {
+        console.log("width less than 1200px")
+
+        // $("#Col3").hide();
+        // // $("#Col1#Col2").removeClass()
+        // $(".Redirect").click(function () {
+        //     $(this).addClass("")
+        // })
+        // $(".Redirect").mouseout(function () {
+        //     $(this).removeClass("")
+        // })
+    
+    
+        // $(".Link").addClass("text-decoration-underline")
+        // $(".LinkInverse").addClass("border-1 border-warning border-bottom")  
+        
+        // $("#Col3Move").removeClass("sticky-top")
+        $("#Col3Move").appendTo("#Col1Col3");
+        $("#Col3").empty();
+    }
+
+
+
+
+
+    $(".Redirect").mouseover(function () {
+        $(this).addClass("")
+    })
+    $(".Redirect").mouseout(function () {
+        $(this).removeClass("")
+    })
+
+
+    $(".Link").mouseover(function () {
+        $(this).addClass("text-decoration-underline")
+    })
+
+    $(".Link").mouseout(function () {
+        $(this).removeClass("text-decoration-underline")
+    })
+
+    $(".LinkInverse").mouseover(function () {
+        $(this).addClass("border-2 border-warning border-bottom")
+    })
+
+    $(".LinkInverse").mouseout(function () {
+        $(this).removeClass("border-2 border-warning border-bottom")
+    })
+
+})
