b9373157629ebea6ec233d9f96e0c6ff10fa09d4
deaxparadox <nitish136900kushwaha@gmail.com>
2024-03-15 13:33:01 +0530

    updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     { path: "dashboard", component: DashboardComponent},
-    { path: "", redirectTo: "/dashboard", pathMatch: 'full'}
+    { path: "", redirectTo: "/home", pathMatch: 'full'},
+    { path: "home", component: HomeComponent}
 ];
